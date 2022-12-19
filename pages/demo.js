e5a9fdd4e906cb64ae3b262c231f289abee182b3
Tyler Heathcote <tyler.j.heathcote@outlook.com>
2022-12-19 10:20:25 -0500

    added server functionality

--- pages/demo.js
+++ pages/demo.js
@@ -2,12 +2,20 @@ import NavBar from '../components/navbar';
 import BarChart from '../components/barChart';
 
 function Demo() {
+  function click() {
+    console.log('spaghetti');
+    fetch('/demo-request')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <>
       <NavBar />
       <div id="content">
         <h1>Look at how fast QLache is!</h1>
         <BarChart />
+        <button onClick={click}>click me</button>
       </div>
     </>
   );
